dbe6db9a18cdf6aa8b2d41d9b7254d96828ce869
FaheemOnHub <faheemmushtaq89@gmail.com>
2023-10-05 21:41:54 +0530

    🚀Implemented-the-webpage-new-quote-function

---
+++ script.js
@@ -0,0 +1,26 @@
+const quoteText = document.querySelector(".quote");
+const quoteBtn = document.querySelector("button");
+const named = document.querySelector(".name");
+const copy = document.querySelector(".copy");
+const twitter = document.querySelector(".twitter");
+
+randomQuote = () => {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "loading...";
+  fetch("https://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      quoteText.textContent = result.content;
+      named.textContent = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
+    });
+};
+randomQuote();
+quoteBtn.addEventListener("click", () => {
+  randomQuote();
+});
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+});
